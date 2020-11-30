cc24b4ea5b8594642e9d46a42e0294b1025ea8f0
Rodrigo F Martins <martins.drg@gmail.com>
2020-11-30 15:37:05 -0300

    fix: missing runner token validation

--- startup.sh
+++ startup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo "Starting supervisor (Docker)"
 sudo service docker start
 
@@ -8,7 +7,6 @@ if [ -n "${GITHUB_REPOSITORY}" ]
 then
     auth_url="https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token"
     registration_url="https://github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY}"
-
 else
     auth_url="https://api.github.com/orgs/${GITHUB_OWNER}/actions/runners/registration-token"
     registration_url="https://github.com/${GITHUB_OWNER}"
@@ -18,6 +16,12 @@ echo "Requesting registration URL at '${auth_url}'"
 
 payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PERSONAL_TOKEN}" ${auth_url})
 export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
+
+if [ "${RUNNER_TOKEN}" == "null" ]
+then
+    echo "${payload}"
+    exit 1
+fi
 
 ./config.sh \
     --name ${RUNNER_NAME}-$(hostname) \
